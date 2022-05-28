062ddebbc5318efef94c846d5935d4fc46e0faa4
ParkJuhyun <wngus6429@gmail.com>
2022-05-28 15:49:57 +0900

    update fullstack

--- projects/07_flask_ABTest_Practice_DB/blog_abtest.py
+++ projects/07_flask_ABTest_Practice_DB/blog_abtest.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template, make_response
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from flask_cors import CORS
 from blog_view import blog
+from blog_control.user_mgmt import User
 import os
 
 # https 만을 지원하는 기능을 http 에서 테스트할 때 필요한 설정
@@ -9,7 +10,7 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 app = Flask(__name__, static_url_path='/static')
 CORS(app)
-app.secure_key = 'dave_server'
+app.secret_key = 'dave_server1'
 
 app.register_blueprint(blog.blog_abtest, url_prefix='/blog')
 login_manager = LoginManager()
