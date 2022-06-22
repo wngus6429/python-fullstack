ec8173547f9576a4a4754c4142e96da9130b5751
ParkJuhyun <wngus6429@gmail.com>
2022-06-22 20:15:22 +0900

    update part2 complete

--- part2make/js/main.js
+++ part2make/js/main.js
@@ -2,6 +2,10 @@ const backToTop = document.getElementById('backtotop');
 
 function checkScroll() {
   // 웹 페이지가 수직으로 얼마나 스크롤되어 있는지.
+  /*
+      웹페이지가 수직으로 얼마나 스크롤되었는지를 확인하는 값(픽셀 단위로 반환)
+      https://developer.mozilla.org/ko/docs/Web/API/Window/pageYOffset
+    */
   let pageYoffset = window.pageYOffset;
   if (pageYoffset !== 0) {
     backToTop.classList.add('show');
@@ -12,6 +16,10 @@ function checkScroll() {
 
 function moveBackToTop() {
   if (window.pageYOffset > 0) {
+    /*
+        smooth 하게 스크롤하기
+        https://developer.mozilla.org/en-US/docs/Web/API/Element/scrollTo
+        */
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
@@ -19,6 +27,36 @@ window.addEventListener('scroll', checkScroll);
 backToTop.addEventListener('click', moveBackToTop);
 
 // ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+function transformNext(event) {
+  const slideNext = event.target;
+  const slidePrev = slideNext.previousElementSibling;
+
+  const classList = slideNext.parentElement.parentElement.nextElementSibling;
+  let activeLi = classList.getAttribute('data-position');
+  const liList = classList.getElementsByTagName('li');
+
+  // 하나의 카드라도 왼쪽으로 이동했다면, 오른쪽으로 갈 수 있음
+  if (Number(activeLi) < 0) {
+    activeLi = Number(activeLi) + 260;
+
+    // 왼쪽에 있던 카드가 오른쪽으로 갔다면, 다시 왼쪽으로 갈 수 있으므로 PREV 버튼 활성화
+    slidePrev.style.color = '#2f3059';
+    slidePrev.classList.add('slide-prev-hover');
+    slidePrev.addEventListener('click', transformPrev);
+
+    // 맨 왼쪽에 현재 보이는 카드가, 맨 첫번째 카드라면, 오른쪽 즉, NEXT 로 갈 수 없으므로 NEXT 버튼 비활성화
+    if (Number(activeLi) === 0) {
+      slideNext.style.color = '#cfd8dc';
+      slideNext.classList.remove('slide-next-hover');
+      slideNext.removeEventListener('click', transformNext);
+    }
+  }
+
+  classList.style.transition = 'transform 1s';
+  classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
+  classList.setAttribute('data-position', activeLi);
+}
+
 function transformPrev(event) {
   const slidePrev = event.target;
   const slideNext = slidePrev.nextElementSibling;
@@ -43,10 +81,11 @@ function transformPrev(event) {
     if (classList.clientWidth > liList.length * 260 + Number(activeLi)) {
       slidePrev.style.color = '#cfd8dc';
       slidePrev.classList.remove('slide-prev-hover');
+      slidePrev.removeEventListener('click', transformPrev);
     }
-
     slideNext.style.color = '#2f3059';
     slideNext.classList.add('slide-next-hover');
+    slideNext.addEventListener('click', transformNext);
   }
   classList.style.transition = 'transform 1s';
   classList.style.transform = 'translateX(' + String(activeLi) + 'px)';
